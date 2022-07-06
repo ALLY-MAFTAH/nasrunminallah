d0c8cf2576929fa14d93cb82aab5d7a0734ca802
unknown <ally.maftah@tangent-technologies.net>
2022-07-06 17:35:17 +0300

    Initial serve settings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Masjid Nasrun Minallah | Live duruus</title>
+    <title>Masjid Nasrun Minallah | Home</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/images/NEW LOGO.jpg" rel="shortcut icon" />
@@ -37,86 +37,10 @@
                     <img style="max-width: 100%;" src="{{ asset('assets/images/NEW LOGO.jpg') }}" alt="">
                 </div>
                 <img src="{{ asset('assets/images/online.png') }}" width="300px" alt="">
-                <h2><span style="color:rgb(66, 39, 5);font-weight:bolder" class="element"></span></h2>
-                <h1 class="border-bottom mx-5" style="color:white;"></h1>
-                <table id="info">
-                    @if ($broadcast->status == true)
-                        <h6 class="" style="color: rgb(7, 6, 62);"><b id="info1">{{ $broadcast->title }}</b>
-                        </h6>
-                        <h6 class="" style="color: rgb(7, 56, 9);"><b id="info2">{{ $broadcast->book }}</b>
-                        </h6>
-                        <p style="color: rgb(54, 24, 252);"> <b id="info3">{{ $broadcast->teacher }}</b></p>
-                        <h6 id="info4" style="color:red">{{ $broadcast->extra }}</h6>
-                    @else
-                        <br>
-                        <h3 class="text-danger"><b id="info5">HATUPO LIVE MUDA HUU</b></h3>
-                        <br>
-                        <p id="info6">Endelea Kusikiliza Radio</p>
-                    @endif
-                </table>
-                <div style="padding-top: 20px; padding-bottom:10px">
-                    <audio controls autoplay>
-                        <source src="http://stream.zeno.fm/0gpzrz1trfhvv">
-                    </audio>
-                </div>
-                @if ($broadcast->status == true)
-                    <div class="questions">
-                        <div class="row pt-2 px-3">
-                            <div class="col-9 text-start"><a style="color: black; font-size:15px">Sehemu ya Maswali</a>
-                            </div>
-                            <div class="col-3 text-end">
-                                <div class="dropdown">
-                                    <a class="" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
-                                        aria-expanded="false">
-                                        <i style="color: black;font-size:15px" class="fas fa-ellipsis-v"></i>
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal"
-                                                data-bs-target="#questionsModal">Maswali Yaliyoulizwa</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <form action="{{ route('add_question') }}" method="post">
-                                @csrf
-                                <div class="form-group pb-1">
-                                    <input class="form-control input-group" name="name" autocomplete="name"
-                                        placeholder="Jina" required>
-                                </div>
-                                <div class="form-group">
-                                    <textarea class="form-control input-group" name="qn" rows="3" id="qn" placeholder="Swali" required></textarea>
-                                </div>
-                                <div class="form-group py-2">
-                                    <button class="btn btn-sm btn-outline-success" type="submit">Tuma</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                @endif
+
             </div>
         </div>
-        <div class="modal fade text-left" id="questionsModal">
-            <div class="modal-dialog modal-md">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Maswali Yaliyoulizwa ({{ $questions->count() }})</h5>
-                        <button class="btn btn-outline-danger close" data-bs-dismiss="modal">
-                            <span>X</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="">
-                            @foreach ($questions as $question)
-                                <div style="color: rgb(6, 117, 17); font-weight:bold">{{ $question->name }}</div>
-                                <div style="font-size: 14px">{{ $question->qn }}</div>
-                                <hr>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+       
         <!--====== Javascripts & Jquery ======-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js">
         <script src="{{asset('js/jquery-2.1.4.min.js')}}"></script>
